7bdc72e7fc1dd30700e9b9de363545d6f3abe38b
chris <christopherscaudill@gmail.com>
2019-02-07 00:30:54 -0500

    best for now

--- PDGfast/Program.cs
+++ PDGfast/Program.cs
@@ -8,48 +8,52 @@ namespace PDGfast
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
             Console.WriteLine($"Start");
-            Run();
-        }
 
-        static void Run()
-        {
-            var BestTotal = 0;
-            var BestRolls = 0;
-            var counter = 0;
-            var gp = 0;
-            var GamesToPlay = 10000000000;
+
+            Random rnd = new Random();
+            int BestTotal = 0;
+            int BestRolls = 0;
+            int counter = 0;
+            int gp = 0;
+            long GamesToPlay = 10000000000;
+
+
+
             while (counter++ < GamesToPlay)
             {
-                var (Total, Rolls) = RunPigDiceGame();
-                gp++;
+                int Total = 0;
+                int Rolls = 0;
+                int Die = 0;
+
+                do
+                {
+                    Total += Die;
+                    Die = rnd.Next(6) + 1;
+                    Rolls++;
+                }
+                while (Die != 1);
+
+
                 if (Total > BestTotal)
                 {
                     BestTotal = Total;
                     BestRolls = Rolls;
-                    Console.WriteLine($"Score: {BestTotal} in {BestRolls} rolls. Played Games: {gp}");
+                    Console.WriteLine($"Score: {BestTotal} in {BestRolls} rolls. Played Games: {counter}");
                 }
-                
-            }
-            
-        }
+                //continue;
 
-        static (int, int) RunPigDiceGame()
-        {
-            Random rnd = new Random();
-            var Total = 0;
-            var Rolls = 0;
-            int Die = 0;
 
-            while (Die != 1)
-            {
-                Total += Die;
-                Die = rnd.Next(6) + 1;
-                Rolls++;
+
             }
-            return (Total, Rolls);
+
+            Console.ReadKey();
+
+
         }
     }
 }
